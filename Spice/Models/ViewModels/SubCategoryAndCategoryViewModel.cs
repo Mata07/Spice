46b95f9c8582100f8fe26d16f58c54e3a6ab611b
Mata07 <mmatisic7@gmail.com>
2021-03-01 13:29:38 +0100

    Added SubCategory/Create

---
+++ Spice/Models/ViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models.ViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+        public SubCategory SubCategory { get; set; }
+
+        // We need only List of Names from SubCategory so it's <string> not full SubCategory
+        public List<string> SubCategoryList { get; set; }
+
+        // for error messages
+        public string StatusMessage { get; set; }
+
+    }
+}
