e2f3ba95de94980789336e3a7ed66327cd0bfe08
Mata07 <mmatisic7@gmail.com>
2021-03-05 17:39:39 +0100

    Added SD

---
+++ Spice/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Utility
+{
+    // static details - for constants
+    public static class SD
+    {
+        public const string DefaultFoodImage = "default_food.png";
+    }
+}
